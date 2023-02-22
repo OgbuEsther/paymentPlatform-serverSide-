ce8a436d8146a1b133d7e13cbdd5714b5ff73a64
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-22 14:38:11 +0100

    saved

--- models/userModel.ts
+++ models/userModel.ts
@@ -3,42 +3,56 @@ import { userData } from "../interfaces/userInterfaces";
 
 interface user extends userData, mongoose.Document {}
 
-const userSchema = new mongoose.Schema<userData>({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-  },
-  accountNumber: {
-    type: Number,
-  },
-  userName: {
-    type: String,
-  },
-
-  verified: {
-    type: Boolean,
-  },
-  wallet: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "walletCollections",
+const userSchema = new mongoose.Schema<userData>(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-  ],
-  history: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "historyCollections",
-    },
-  ],
-});
 
-const userModel = mongoose.model<user>("");
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    accountNumber: {
+      type: Number,
+    },
+
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+
+    userName: {
+      type: String,
+      required: true,
+    },
+
+    verified: {
+      type: Boolean,
+    },
+    wallet: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "walletCollections",
+      },
+    ],
+    history: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "historyCollections",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const userModel = mongoose.model<user>("userCollctions", userSchema);
