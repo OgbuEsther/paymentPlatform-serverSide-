dbe0d6adcf10ecaccbfccf494d534f704566aedf
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-22 14:29:10 +0100

    saved

---
+++ interfaces/userInterfaces.ts
@@ -0,0 +1,11 @@
+export interface userData {
+  name: string;
+  email: string;
+  password: string;
+  accountNumber: number;
+  phoneNumber: number;
+  userName: string;
+  verified: boolean;
+  wallet: {}[];
+  history: {}[];
+}
--- models/userModel.ts
+++ models/userModel.ts
@@ -1,3 +1,23 @@
 import mongoose from "mongoose";
+import { userData } from "../interfaces/userInterfaces";
 
-const userSchema = new mongoose.Schema({});
+interface user extends userData, mongoose.Document {}
+
+const userSchema = new mongoose.Schema<userData>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  accountNumber: {
+    type: Number,
+  },
+  userName: {
+    type: String,
+  },
+});
