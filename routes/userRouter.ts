fb44162bde40b1c047fe7d06f9736636f48659b7
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-22 17:36:00 +0100

    saved

--- routes/userRouter.ts
+++ routes/userRouter.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { MakeTransfer, registerUser } from "../controller/userControls";
+import {
+  getAllUsers,
+  MakeTransfer,
+  registerUser,
+} from "../controller/userControls";
 
 const userRouter = express.Router();
 
 userRouter.route("/signup").post(registerUser);
 userRouter.post("/transaction/:userID/:walletID", MakeTransfer);
+userRouter.get("/", getAllUsers);
 
 export default userRouter;
