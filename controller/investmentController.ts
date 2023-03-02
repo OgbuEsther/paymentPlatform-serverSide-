cc61290cdcc228a5147f55f358d3517af86330c3
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-02 13:28:32 +0100

    saved

--- controller/investmentController.ts
+++ controller/investmentController.ts
@@ -2,3 +2,19 @@ import { Request, Response } from "express";
 import mongoose from "mongoose";
 import investModel from "../models/DASHBOARD/invest/investModel";
 import investorModel from "../models/DASHBOARD/invest/investors";
+import userModel from "../models/userModel";
+
+export const createInvestify = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const getUser = await userModel.findById(req.params.id);
+
+  const getDate = new Date().toDateString();
+
+  if (getUser?.isAdmin === true) {
+    const {} = req.body;
+
+    const creating = await investModel.create({});
+  }
+};
