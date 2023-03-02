315626f1c767c5e0e6fbbecf0a5dfe0685cd93ef
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-02 03:48:23 +0100

    saved

---
+++ models/DASHBOARD/invest/investModel.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import { invest } from "../../../interfaces/dashBoardInterfaces";
+
+interface investData extends invest, mongoose.Document {}
+
+const investSchema = new mongoose.Schema<invest>(
+  {
+    title: {
+      type: String,
+    },
+    status: {
+      type: Boolean,
+    },
+    startTime: {
+      type: String,
+    },
+    percentageInterest: {
+      type: Number,
+    },
+    duration: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    totalUnit: {
+      type: Number,
+    },
+    description: {
+      type: String,
+    },
+    amountPerUnit: {
+      type: Number,
+    },
+    investors: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "investorsCollections",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const investModel = mongoose.model<investData>(
+  "investCollections",
+  investSchema
+);
