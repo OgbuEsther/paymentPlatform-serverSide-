41013868a6318113a2538b2a7ee72bc99c1af3d0
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-02 04:24:08 +0100

    saved

---
+++ controller/investmentController.ts
@@ -0,0 +1,4 @@
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+import investModel from "../models/DASHBOARD/invest/investModel";
+import investorModel from "../models/DASHBOARD/invest/investors";
