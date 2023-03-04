5f7a13891ed16323c4ef4580bb8471748ec06203
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-04 15:41:31 +0100

    saved

--- interfaces/walletInterfaces.ts
+++ interfaces/walletInterfaces.ts
@@ -6,4 +6,5 @@ export interface walletData {
   saveLock: {}[];
   target: {}[];
   invest: {}[];
+  myInvestment: [];
 }
--- models/walletModel.ts
+++ models/walletModel.ts
@@ -40,6 +40,7 @@ const walletSchema = new mongoose.Schema<walletData>(
         ref: "investCollections",
       },
     ],
+    myInvestment: [],
   },
   { timestamps: true }
 );
